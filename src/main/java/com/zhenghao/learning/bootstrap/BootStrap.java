b02100cce8189098ea7c27d42706d3750b480dc5
jiangzhenghao <loveair007@139.com>
2018-05-28 17:00:09 +0800

    1:集成sharding-jdbc

--- src/main/java/com/zhenghao/learning/bootstrap/BootStrap.java
+++ src/main/java/com/zhenghao/learning/bootstrap/BootStrap.java
@@ -1,17 +1,23 @@
 package com.zhenghao.learning.bootstrap;
 
+import com.zhenghao.learning.sharding.service.DemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author jiangzhenghao
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.zhenghao.learning.sharding"})
 public class BootStrap
 {
 
   public static void main(String[] args) {
-    SpringApplication.run(BootStrap.class, args);
+    ApplicationContext applicationContext = SpringApplication.run(BootStrap.class, args);
+    applicationContext.getBean(DemoService.class).demo();
   }
 }
